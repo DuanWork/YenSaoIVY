75037833620fe8dabe91f98ed6fd2d2552572f8a
DuanWork <dinhduan.work@gmail.com>
2022-02-22 12:43:57 +0700

    Update

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,6 +12,19 @@ function show_hide()
     }
 }
 
+var b;
+function show_menu()
+{
+    if (b == 1)
+    {
+        document.getElementById("list").style.display="block";
+        return b = 0;
+    }else{
+        document.getElementById("list").style.display="none";
+        return b = 1;
+    }
+}
+
 function Left() {
     const boxes = document.querySelectorAll(".box");
     const tmpNode = boxes[0];
@@ -53,4 +66,34 @@ function Right() {
         boxes[3].className = "box move-to-position5-from-right";
     }, 500);
 
+}
+
+
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("white", "");
+  }
+  x[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += "white";
 }
